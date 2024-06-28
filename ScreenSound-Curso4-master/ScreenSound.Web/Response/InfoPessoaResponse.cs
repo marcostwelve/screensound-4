9e6e899125b0dfae9593842afac744f315796670
marcostwelve <marcosnyan12@gmail.com>
2024-06-28 19:36:46 -0300

    Autenticação ScreenSound

---
+++ ScreenSound-Curso4-master/ScreenSound.Web/Response/InfoPessoaResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ScreenSound.Web.Response
+{
+    public class InfoPessoaResponse
+    {
+        public string Email { get; set; }
+        public bool isEmailConfirmed { get; set; }
+    }
+}
